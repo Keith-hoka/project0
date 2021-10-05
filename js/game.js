8e60f860c4893862f6890d53ab6bdd19eda41409
Keith_hoka <keith_d_hau@hotmail.com>
2021-10-06 01:41:52 +1100

    v2.0_Tic Tac Toe Game

--- js/game.js
+++ js/game.js
@@ -12,62 +12,114 @@ const winning = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [2,4,6]
+  [2,4,6],
 ];
+
 let winRoundOfX = 0;
 let winRoundOfO = 0;
 let prevWinX = localStorage.getItem("roundsX");
 let prevWinO = localStorage.getItem("roundsO");
+
+$("#start-btn").click(function(){
+    $("#start-btn").attr("class", "enterNameBtn-hide");
+    $("form").removeClass();
+    $("#change-btn").attr("class", "changeNameBtn-hide");
+});
+
+$(".submitBtn").click(function(){
+  const cusName = $(".name").val();
+  localStorage.setItem("name", cusName);
+  $("form").addClass("player-hide");
+  $("#winnerx").text(`Player ${cusName} win: 0`);
+  $("#winnero").text(`Player O win: 0`);
+  $("#change-btn").attr("class", "changeNameBtn");
+  $("#start-btn").attr("class", "enterNameBtn");
+});
+
+$("#change-btn").click(function(){
+  localStorage.clear();
+  location.reload();
+});
+
+let name = localStorage.getItem("name");
+
+if (prevWinX === null){
+  if(name === null) {
+    $("#winnerx").text(`Player X win: 0`);
+  } else {
+    $("#winnerx").text(`Player ${name} win: 0`);
+  }
+} else {
+  if(name === null) {
+    $("#winnerx").text(`Player X win: ${prevWinX}`);
+  } else {
+    $("#winnerx").text(`Player ${name} win: ${prevWinX}`);
+  }
+}
+
+if (prevWinO === null){
+  $("#winnero").text(`Player O win: 0`);
+} else {
+  $("#winnero").text(`Player O win: ${prevWinO}`);
+}
 
 $(".grid-item").each(function(){
   $(this).click(function(){
     $(this).text(currentPlayer);
     if (currentPlayer === "X") {
       clickedArrX.push($(this).index());
+      console.log(clickedArrX);
     } else {
       clickedArrO.push($(this).index());
+      console.log(clickedArrO);
     }
     toggle();
     for (let i = 0; i < winning.length; i++) {
       for (let n = 0; n < clickedArr.length; n++){
-        if (JSON.stringify(clickedArr[n].sort()) === JSON.stringify(winning[i])){
+        // if (JSON.stringify(clickedArr[n].sort()) === JSON.stringify(winning[i])){
+        if (clickedArrO.length + clickedArrX.length < 9 && winning[i].every(elem => clickedArr[n].includes(elem))){
+          console.log(winning[i]);
+          console.log(clickedArr[n]);
           if (n === 0){
-            console.log("winner is X");
-            $(".grid-container").remove();
-            $(".result").append("<p>Congratulations! Winner is X!</p>");
             winRoundOfX = +prevWinX + 1;
-            console.log(winRoundOfX);
             localStorage.setItem("roundsX", winRoundOfX);
-            console.log(winRoundOfX);
-          } else {
-            console.log("winner is O");
             $(".grid-container").remove();
-            $(".result").append("<p>Congratulations! Winner is O!</p>");
-            winRoundOfO = prevWinO + 1;
+            $(".winner").remove();
+            $("#start-btn").attr("class", "enterNameBtn-hide");
+            if (name === null) {
+              $(".result").append(`<p>Congratulations! You win your first game!</p>`);
+            } else {
+              $(".result").append(`<p>Congratulations! Winner is ${name}!</p>`);
+            }
+          } else {
+            winRoundOfO = +prevWinO + 1;
             localStorage.setItem("roundsO", winRoundOfO);
-            console.log(winRoundOfO);
+            $(".grid-container").remove();
+            $(".winner").remove();
+            $("#start-btn").attr("class", "enterNameBtn-hide");
+            $(".result").append("<p>Congratulations! Winner is O!</p>");
           }
-        } else if (clickedArrO.length + clickedArrX.length === 9){
-          console.log("It is a tie!");
-          $(".grid-container").remove();
-          $(".result").append("<p>It is a Tie! No one wins</p>");
         }
       }
     }
+    if (clickedArrO.length + clickedArrX.length === 9){
+            $(".grid-container").remove();
+            $(".winner").remove();
+            $("#start-btn").attr("class", "enterNameBtn-hide");
+            $(".result").append("<p>It is a Tie! No one wins</p>");
+    }
     // computerPlay($(this).index());
   });
-});
+}) ;
 
 const toggle = function(){
   currentPlayer = currentPlayer === "X" ? "O" : "X";
   return currentPlayer;
 };
 
-$("button").click(function(){
+$("#game-btn").click(function(){
   location.reload();
 });
-
-
 
 // const computerPlay = function(index) {
 //   const randomIndex = Math.round(Math.random() * 8);
